c60b5d2116af6cd06aea4beae76f0cc5661db11d
Tomer <gmtomer@gmail.com>
2015-12-09 11:38:51 +0200

    cli stubsssss

--- src/cli.js
+++ src/cli.js
@@ -1,1 +1,11 @@
 #! /usr/bin/env node
+
+import getFileContents from './index';
+
+getFileContents(process.argv[2], (err, contents) => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log(contents);
+    }
+});
